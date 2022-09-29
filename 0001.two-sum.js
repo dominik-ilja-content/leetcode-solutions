c971d1736acfe81d07a297954b285682fadb925a
Dominik Ilja <104933651+Dominik-Ilja@users.noreply.github.com>
2022-09-28 20:13:21 -0400

    Leetcode 1 - Two Sum

---
+++ 0001.two-sum.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  // store numbers without pairs in map
+  const map = {};
+
+  // look at each of the values in nums
+  for (let i = 0; i < nums.length; i++) {
+    const curr = nums[i];
+    const pair = target - curr;
+
+    // check if the pair is in the map
+    // if it is return the indexes inside an array
+    // else add the curr number as a key and its index as its value 
+    // that way we can return it later
+    if (map[pair] !== undefined) {
+      return [map[pair], i];
+    }
+    else {
+      map[curr] = i;
+    }
+  }
+
+};
+
+console.log(twoSum([1, 98, 7, 54, 2], 3));
