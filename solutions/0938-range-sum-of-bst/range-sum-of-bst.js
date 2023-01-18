8e29f420d08365e39dfbf7391b87fe706f6b3dad
Dominik Ilja <104933651+Dominik-Ilja@users.noreply.github.com>
2023-01-18 11:57:06 -0500

    feat: Add leetcode 0938 with tests

---
+++ solutions/0938-range-sum-of-bst/range-sum-of-bst.js
@@ -0,0 +1,84 @@
+const {
+  TreeNode,
+  createBinarySearchTree,
+} = require("../../data_structures/Trees/Trees");
+
+/**
+ * @param {TreeNode} root
+ * @param {number} low
+ * @param {number} high
+ * @return {number}
+ */
+
+// Depth First Search - In Order (Recursion)
+var rangeSumBST = function (root, low, high) {
+  let sum = 0;
+
+  function traverse(root) {
+    if (root === null) return;
+
+    traverse(root.left);
+
+    if (root.val >= low && root.val <= high) {
+      sum += root.val;
+    }
+
+    traverse(root.right);
+  }
+  traverse(root);
+
+  return sum;
+};
+
+// Breadth First Search (Iterative)
+
+/**
+ * @param {TreeNode} root
+ * @param {number} low
+ * @param {number} high
+ * @return {number}
+ */
+var rangeSumBST = function (root, low, high) {
+  const stack = [root];
+  let sum = 0;
+
+  while (stack.length !== 0) {
+    const { val, left, right } = stack.pop();
+
+    if (val >= low && val <= high) {
+      sum += val;
+    }
+
+    if (left) stack.push(left);
+    if (right) stack.push(right);
+  }
+
+  return sum;
+};
+
+// const space = "  ";
+// console.log(
+//   JSON.stringify(
+//     rangeSumBST(createBinarySearchTree([10, 5, 15, 3, 7, 18]), 7, 15),
+//     null,
+//     space
+//   )
+// ); // 32
+
+// console.log(
+//   JSON.stringify(
+//     rangeSumBST(createBinarySearchTree([10, 5, 15, 3, 7, 13, 18, 1, 6]), 6, 10),
+//     null,
+//     space
+//   )
+// ); // 23
+
+// console.log(
+//   JSON.stringify(
+//     rangeSumBST(createBinarySearchTree([22, 3, 9, 72, 45, 7, 11]), 4, 37),
+//     null,
+//     space
+//   )
+// ); // 49
+
+module.exports = rangeSumBST;
