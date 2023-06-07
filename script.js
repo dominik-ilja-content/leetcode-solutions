3965fe2a8698dbc49d751abbf31cc5c6c85c1e92
Dominik <dominik.ilja.developer@gmail.com>
2023-06-07 05:57:47 -0400

    feat: Remove script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const date = new Date(2023, 4, 26);
-const dates = [];
-
-while (date.getFullYear() < 2025) {
-  dates.push(new Date(date.getFullYear(), date.getMonth(), date.getDate()));
-  date.setDate(date.getDate() + 14);
-}
-
-console.dir(dates);
