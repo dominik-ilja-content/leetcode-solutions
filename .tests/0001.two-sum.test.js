3ff426f4d7335dec28d3f75a408a90d0ee934170
Dominik Ilja <104933651+Dominik-Ilja@users.noreply.github.com>
2023-06-18 12:10:06 -0400

    Update: Remove unnecessary files

--- .tests/0001.two-sum.test.js
+++
@@ -1,9 +0,0 @@
-const twoSum = require('../completed/0001.two-sum');
-const expect = require('chai').expect;
-
-
-describe('0001 Two Sum', function () {
-  it('Should return the correct indexes', function () {
-    expect(twoSum([3, 3], 6)).deep.equal([0, 1]);
-  });
-});
--- .unfinished/.add-tests/1693.daily-leads-and-partners.sql
+++
@@ -1,6 +0,0 @@
-SELECT date_id, make_name, 
-count(DISTINCT lead_id) AS unique_leads,
-count(DISTINCT partner_id) AS unique_partners
-FROM dailysales
-GROUP BY make_name, date_id
-ORDER BY make_name, date_id;
